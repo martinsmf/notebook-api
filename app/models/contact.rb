4eda48a3350f27ec44eaf3003be549e1ade18e87
Matheus Fernandes Martins <matheus.martins@rarolabs.com.br>
2022-03-15 20:37:54 -0300

    .

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,17 +1,17 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
-  def author
-    "Matheus"
-  end
+  belongs_to :kind#, optional: true
+  # def author
+  #   "Matheus"
+  # end
 
-  def kind_description
-    self.kind.description
-  end
+  # def kind_description
+  #   self.kind.description
+  # end
 
-  def as_json(options={})
-    super(
-      root: true,
-      methods: [:author, :kind_description]
-    )
-  end
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:author, :kind_description]
+  #   )
+  # end
 end

9e31fc1c7322081af4b66ca8bcc5aa1f14a0f623
martinsmf <martinsmf00@gmail.com>
2022-06-01 23:34:11 -0300

    class 55

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,5 +1,5 @@
 class KindsController < ApplicationController
-  TOKEN = "secret123"
+  # TOKEN = "secret123"
 
   # include ActionController::HttpAuthentication::Basic::ControllerMethods
   # http_basic_authenticate_with name: "jack", password: "secret"
@@ -55,10 +55,13 @@ class KindsController < ApplicationController
     #   USERS[username]
     # end
     authenticate_or_request_with_http_token do |token, options|
-      ActiveSupport::SecurityUtils.secure_compare(
-        ::Digest::SHA256.hexdigest(token),
-        ::Digest::SHA256.hexdigest(TOKEN)
-      )
+      hmac_secret = "my$ecretK3y"
+      JWT.decode token, hmac_secret, true, { :algorithm => "HS256" }
+      #   ActiveSupport::SecurityUtils.secure_compare(
+      #     ::Digest::SHA256.hexdigest(token),
+      #     ::Digest::SHA256.hexdigest(TOKEN)
+      # )
+
     end
   end
 
